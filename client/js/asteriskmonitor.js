84d1ea8a824d083ad7eaeed927a4ff6bd4d0e5df
Will Reiske <wreiske@mieweb.com>
2019-01-22 23:48:20 -0500

    Changed double quotes to single quotes

--- client/js/asteriskmonitor.js
+++ client/js/asteriskmonitor.js
@@ -18,7 +18,7 @@ Handlebars.registerHelper('ami_connected', function () {
 });
 
 Handlebars.registerHelper('firstrun', function () {
-    return ((Counts.get("user-count") === 0) ? true : false);
+    return ((Counts.get('user-count') === 0) ? true : false);
 });
 Handlebars.registerHelper('log', function (obj) {
     console.log(obj.fetch());
@@ -29,7 +29,7 @@ Handlebars.registerHelper('currentRoute', function () {
 });
 
 Template.registerHelper('reactiveTime', function (time) {
-    if (typeof time === "undefined") {
+    if (typeof time === 'undefined') {
         time = this.starmon_timestamp;
     }
     return moment(time).from(TimeSync.serverTime());
@@ -39,7 +39,7 @@ Template.registerHelper('elapsedTime', function (time, time2) {
     return moment(time).from(time2, true);
 });
 Template.registerHelper('elapsedHMS', function (time1 = Date.now(), time2 = Date.now()) {
-    Session.get("secondTicker");
+    Session.get('secondTicker');
     return moment().startOf('day')
         .seconds(Math.abs((time2 - time1) / 1000))
         .format('H:mm:ss');
@@ -50,7 +50,7 @@ Template.registerHelper('hms', function (time = Date.now()) {
         .format('H:mm:ss');
 });
 Handlebars.registerHelper('moment', function (time) {
-    if (typeof time === "undefined") {
+    if (typeof time === 'undefined') {
         time = this.starmon_timestamp;
     }
     return moment(time).from(TimeSync.serverTime());
@@ -80,5 +80,5 @@ Handlebars.registerHelper('md5', function (string) {
     return CryptoJS.MD5(string).toString();
 });
 Meteor.setInterval(function () {
-    Session.set("secondTicker", new Date().getTime());
+    Session.set('secondTicker', new Date().getTime());
 }, 1000);
